8c9ca4bd80b763f2996a53607334952a68d2d7c9
teppei1994667 <teppei.engineer.667@gmail.com>
2023-05-23 23:00:46 +0900

    reduxインストール

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -3,17 +3,31 @@ import { Typography, Button, Box } from "@mui/material";
 
 export const Home = () => {
   const [count, setCount] = useState(0);
-	const countUp = () => {
-		setCount(prev => prev + 1)
-	}
-	const countReset = () => {
-		setCount(prev => prev = 0)
-	}
+  const countUp = () => {
+    setCount((prev) => prev + 1);
+  };
+  const countReset = () => {
+    setCount((prev) => (prev = 0));
+  };
   return (
-    <Box sx={{textAlign: "center"}}>
-      <Button variant="outlined" onClick={countUp}>カウントアップ</Button>
-			<Button variant="outlined" sx={{ marginLeft: "10px" }} onClick={countReset}>リセット</Button>
-      <Typography variant="h6">{count}</Typography>
-    </Box>
+    <>
+      <Typography variant="subtitle1" sx={{ textAlign: "center"}}>これはuseStateを使って実装しました</Typography>
+      <Box sx={{ textAlign: "center", marginTop: "20px" }}>
+        <Button variant="outlined" onClick={countUp}>
+          カウントアップ
+        </Button>
+        <Button
+          variant="outlined"
+          sx={{ marginLeft: "10px" }}
+          onClick={countReset}
+        >
+          リセット
+        </Button>
+        <Typography variant="h6">{count}</Typography>
+      </Box>
+
+			<Typography variant="subtitle1" sx={{ textAlign: "center", marginTop: "50px"}}>これはReduxを使って実装しました</Typography>
+			
+    </>
   );
 };

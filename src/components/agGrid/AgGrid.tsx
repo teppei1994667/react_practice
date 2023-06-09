4b990d5c8158672313dec1afa72b2a2bb7866acd
teppei1994667 <teppei.engineer.667@gmail.com>
2023-06-10 00:08:56 +0900

    AgGridのrowData作成

--- src/components/agGrid/AgGrid.tsx
+++ src/components/agGrid/AgGrid.tsx
@@ -1,11 +1,23 @@
-import { Typography } from "@mui/material";
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+import { Typography } from '@mui/material';
+import { useMemo } from 'react';
 
 export const AgGrid = () => {
+  const rowData = useMemo(() => [
+    {日付: "2023/06/01", 売り上げ: "200000"},
+    {日付: "2023/06/02", 売り上げ: "150000"},
+    {日付: "2023/06/03", 売り上げ: "300000"},
+    {日付: "2023/06/04", 売り上げ: "300000"},
+    {日付: "2023/06/05", 売り上げ: "400000"}
+  ], [])
   return (
     <>
-      <Typography variant="h6">
-        このページはAgGridの練習に使います
+      <Typography variant="h6" sx={{ textAlign: "center"}}>
+        このページはAgGridを実装します
       </Typography>
+
     </>
   );
-};
+},;

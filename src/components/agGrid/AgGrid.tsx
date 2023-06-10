944d520b6ece5185f8288a57988f90a0ecbfd371
teppei1994667 <teppei.engineer.667@gmail.com>
2023-06-10 12:02:07 +0900

    apiでrefにアクセス可能, 見た目修正

--- src/components/agGrid/AgGrid.tsx
+++ src/components/agGrid/AgGrid.tsx
@@ -1,32 +1,45 @@
-import { Grid, Typography } from "@mui/material";
-import { useMemo } from "react";
+import { Box, Grid, Typography } from "@mui/material";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 
-export const AgGrid = () => {
-  const rowData = [
-    { 日付: "2023/06/01", 売り上げ: "200000" },
-    { 日付: "2023/06/02", 売り上げ: "150000" },
-    { 日付: "2023/06/03", 売り上げ: "300000" },
-    { 日付: "2023/06/04", 売り上げ: "300000" },
-    { 日付: "2023/06/05", 売り上げ: "400000" },
-  ];
+export type rowDataType = {
+  日付: string,
+  売り上げ: string
+}
 
+export const AgGrid = () => {
+  const gridRef = useRef<AgGridReact>(null);
+
+  // 外部からのデータ取得を前提に実装している
+  const [rowData, setRowData] = useState<rowDataType[]>()
+  useEffect(() => {
+    setRowData([
+      { 日付: "2023/06/01", 売り上げ: "200000" },
+      { 日付: "2023/06/02", 売り上げ: "150000" },
+      { 日付: "2023/06/03", 売り上げ: "300000" },
+      { 日付: "2023/06/04", 売り上げ: "300000" },
+      { 日付: "2023/06/05", 売り上げ: "400000" },
+    ])
+  }, [rowData])
+
+  // カラム定義
   const columnDefs = useMemo(
-    () => [{ field: "日付", width: 1000 }, { field: "売り上げ", width: 1000 }],
+    () => [{ field: "日付", width: 300 }, { field: "売り上げ", width: 300 }],
     []
   );
+
   return (
-    <>
+    <Box sx={{width: "100%", height: "auto"}}>
       <Typography variant="h6" sx={{ textAlign: "center" }}>
         このページはAgGridを実装します
       </Typography>
-      <Grid container spacing={0.75} sx={{width: "100vw", height: "50vh", marginTop: "50px"}}>
-        <Grid item sx={{width: "100%", height: "100%"}} className="ag-theme-alpine">
-          <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
+      <Grid container sx={{width: "100%", height: "50vh", marginTop: "50px", justifyContent: "center"}}>
+        <Grid item sx={{width: "90%", height: "100%", }} className="ag-theme-alpine">
+          <AgGridReact ref={gridRef} rowData={rowData} columnDefs={columnDefs}></AgGridReact>
         </Grid>
       </Grid>
-    </>
+    </Box>
   );
 };
